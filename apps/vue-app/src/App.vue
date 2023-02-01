b184312bc5b3ffccc7a139cf457f010588c3fd82
Alexandre Monteiro <alexandre.monteiro.bec@gmail.com>
2023-02-01 20:36:02 -0300

    fix vue app binding

--- apps/vue-app/src/App.vue
+++ apps/vue-app/src/App.vue
@@ -1,33 +1,8 @@
 <script setup lang="ts">
-import { MyComponent, MyHeading, MyText, MyInput } from "@stencil-ui/vue"
+import WebComponents from './components/WebComponents.vue'
 </script>
 
 <template>
-  <main class="app">
-    <MyHeading>Vue + Web components</MyHeading>
-
-    <MyText>Type your name:</MyText>
-
-    <MyInput placeholder='Type...'  />
-
-    <MyComponent />
-  </main>
+  <WebComponents />
 </template>
 
-<style scoped>
-.app {
-  display: flex;
-  flex-direction: column;
-  align-items: center;
-  justify-content: center;
-
-  background-color: #242424;
-
-  min-height: 100vh;
-  min-width: 100vw;
-}
-
-#my-component {
-  margin-top: 10px;
-}
-</style>
---
+++ apps/vue-app/src/components/WebComponents.vue
@@ -0,0 +1,48 @@
+<script lang="ts">
+import { MyInput } from '@stencil-ui/vue'
+
+export default {
+  data() {
+    return {
+      name: 'VueJs'
+    }
+  },
+  methods: {
+    changeName (event: any) {
+      const value = event.detail
+
+      this.name = value
+    }
+  }
+}
+</script>
+
+<template>
+  <main class="app">
+    <my-heading>Vue + Web components</my-heading>
+
+    <my-text>Type your name:</my-text>
+
+    <my-input placeholder='Type...' @changeInput="changeName" />
+
+    <my-component :name="name" />
+  </main>
+</template>
+
+<style scoped>
+.app {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+
+  background-color: #242424;
+
+  min-height: 100vh;
+  min-width: 100vw;
+}
+
+#my-component {
+  margin-top: 10px;
+}
+</style>
